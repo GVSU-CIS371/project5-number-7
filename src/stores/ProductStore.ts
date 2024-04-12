3809983533f3740653169dd26c3cfec3b846c7ed
xxaleebxx <abertram2021@gmail.com>
2024-04-11 21:50:10 -0400

    product store

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -3,5 +3,21 @@ import { ProductDoc } from "../types/product";
 import { initProducts } from "../data-init";
 
 export const useProductStore = defineStore("ProductStore", {
-  // your answer
+  // State
+  state: () => ({
+    products: [] as ProductDoc[],
+  }),
+  // Actions
+  actions: {
+    init() {
+      this.products = initProducts;
+    },
+    filterByCategory(category: string) {
+      return this.products.filter((x: ProductDoc) => x.data.category.includes(category));
+
+    },
+    filterByRating(minRating: number) {
+      return this.products.filter( (p: ProductDoc) => p.data.rating > minRating )
+    },
+  },
 });
